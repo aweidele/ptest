6863540ee23732317ba3f15e380fa1ed9ed63a1c
Aaron Weidele <aweidele@gmail.com>
2017-07-19 22:38:55 -0400

    Ok, it's working

--- plugin-test.php
+++ plugin-test.php
@@ -7,77 +7,39 @@ Version: 1.0
 Author: Aaron
 Author URI: http://www.aaronweidele.com
 */
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
 
-class AW_PluginTest {
+$options_page = 'general';
+add_action( 'admin_init', 'register_bn_options' );
+function register_bn_options() {
 
-  private static $instance = null;
-  public $options;
-  public $page = 'plugin-test';
+	global $options_page; /* <-- DELETE THIS LATER */
 
-  public static function init() {
-    if ( null == self::$instance ) {
-      self::$instance = new self;
-    }
-    return self::$instance;
-  }
-
-  private function __construct() {
-    add_action( 'admin_menu', array( &$this, 'add_page' ) );
-    add_action( 'admin_init', array( &$this, 'register_page_options') );
-  }
-
-
-  public function add_page() {
-    add_options_page( 'Theme Options', 'Theme Options', 'manage_options', $this->page, array( $this, 'display_page' ) );
-  }
-
-  public function display_page() {
-    ?>
-    <div class="wrap">
-        <h2>Theme Options</h2>
-        <form method="post" action="options.php">
-        <?php
-            settings_fields($this->page);
-            do_settings_sections($this->page);
-            submit_button();
-        ?>
-        </form>
-    </div> <!-- /wrap -->
-
-    <?php
-  }
-
-  public function register_page_options() {
-
-    // Add Section for option fields
-    add_settings_section( 'pltest_section', 'Theme Options', array( $this, 'display_section' ), $this->page ); // id, title, display cb, page
-
-    // Add Title Field
-    add_settings_field( 'pltest_title_field', 'Blog Title', array( $this, 'title_settings_field' ), $this->page, 'pltest_section' ); // id, title, display cb, page, section
-
-    // Add Background Color Field
-    add_settings_field( 'pltest_bg_field', 'Background Color', array( $this, 'bg_settings_field' ), $this->page, 'pltest_section' ); // id, title, display cb, page, section
-
-    // Register Settings
-    register_setting( $this->page, 'pltest_settings_options', array( $this, 'validate_options' ) ); // option group, option name, sanitize cb
-  }
-
-  public function title_settings_field() {
-
-    $val = ( isset( $this->options['title'] ) ) ? $this->options['title'] : '';
-    echo '<input type="text" name="cpa_settings_options[title]" value="' . $val . '" />';
-  }
-
-  public function bg_settings_field() {
-
-    $val = ( isset( $this->options['title'] ) ) ? $this->options['background'] : '';
-    echo '<input type="text" name="cpa_settings_options[background]" value="' . $val . '" class="cpa-color-picker" >';
-
-  }
-
+	add_settings_section(
+		'pt_settings',						// ID used to identify this section and with which to register options
+        'PT Settings', 		                // Title to be displayed on the administration page
+        'pt_settings_callback_function', 	// Callback used to render the description of the section
+        $options_page			  			// Page on which to add this section of options <--- NOTE we're going to change this
+	);
+	add_settings_field( 
+		'bn_title',							// ID used to identify the field throughout the theme
+		'Breaking News Title',				// Label for the feild
+		'bn_title_callback_function',		// The name of the function responsible for rendering the option interface
+		$options_page,						// The page on which this option will be displayed
+		'pt_settings',						// The name of the section to which this field belongs (Same as first setting in add_settings_section()
+		array(								// Not exactly sure what this is? We'll find out
+			'What is this?',
+		)
+	);
+	register_setting( $options_page , 'bn_title' );
 }
 
-AW_PluginTest::init();
-//$aw_plugintest = new AW_PluginTest;
+//// THIS FUNCTION REFERENCES THE THIRD SETTING IN add_settings_section
+function pt_settings_callback_function() {
+	//echo 'FUCK YOU';
+}
+
+//// THIS FUNCTION REFERENCES THE THIRD SETTING IN add_settings_field
+function bn_title_callback_function( $args ) {
+	$html = '<input type="text" id="bn_title" name="bn_title" value="' . get_option('bn_title') . '" class="regular-text code">';	
+	echo $html;
+}
